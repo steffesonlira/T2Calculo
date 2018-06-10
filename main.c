ee86edae98a878f60dd162ad0a6606e47a168ace
haze120 <31262457+haze120@users.noreply.github.com>
2018-06-10 16:06:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -13,29 +13,26 @@ int main()
     setlocale(LC_ALL, "Portuguese");
 
     int op,opf;
-    double k,x;// variáveis usadas na opção Derivadas
-    double a,b; //variáveis usadas na opção Integral
-    double x0; //Variável usada na opção Zero da Função
+    double k,x;// variÃ¡veis usadas na opÃ§Ã£o Derivadas
+    double a,b; //variÃ¡veis usadas na opÃ§Ã£o Integral
+    double x0; //VariÃ¡vel usada na opÃ§Ã£o Zero da FunÃ§Ã£o
     double result;
-    double * resultzero;
-
+    double resultzero;
+    char final;
+    
  do{
-    printf(" Escolha uma das opções abaixo: ");
+ do{
+    printf(" Escolha uma das opÃ§Ãµes abaixo: ");
     printf("\n 1 - Derivadas");
     printf("\n 2 - Integral");
-    printf("\n 3 - Zero da função.");
+    printf("\n 3 - Zero da funÃ§Ã£o.");
     printf("\n 4 - Sair\n");
     printf(" ");
         scanf("%d", &op);
-    switch(op){
+            system("cls");
+                if (op==4){exit(0);}
 
-case 1:
-
-     printf("\n Digite o valor de X:\n ");
-        scanf("%f", &x);
-
-
-    printf("\n Escolha a Função abaixo: ");
+    printf("\n Escolha a FunÃ§Ã£o abaixo: ");
     printf("\n 1 - f(x) = k");
     printf("\n 2 - f(x) = x^k");
     printf("\n 3 - f(x) = k^x");
@@ -47,73 +44,61 @@ case 1:
     printf("\n 9 - f(x) = tg(x)");
     printf("\n 10 - f(x) = sqrt(x)");
     printf("\n 11 - f(x) = 1/x");
+    printf("\n12 - Voltar ao menu opÃ§Ãµes.");
+    printf("\n13 - Sair.");
     printf("\n ");
         scanf("%d", &opf);
-
+            system("cls");
+                if (opf==13){exit(0);}
+}while (opf==12);
+    
+    if(op==1){
+        printf("\n Digite o valor de X:\n ");
+            scanf("%f", &x); 
+   
     if(opf==1 || opf==2 || opf==3 || opf==5){
-    printf("\n Digite o valor de K:\n ");
-        scanf("%f", &k);
-    }
+        printf("\n Digite o valor de K:\n ");
+            scanf("%f", &k);}
+        
     if(opf==1){
         x = 0;
-    }
-
+    
     result = Derivada(x,opf,k);
     printf("\n Derivada = %f \n", result);
-    break;
-
-case 2:
-    printf("\n Digite o valor de A:\n ");
-        scanf("%f", &a);
-    printf(" Digite o valor de B:\n ");
-        scanf("%f", &b);
-
-    printf("\n Escolha a Função abaixo: ");
-    printf("\n 1 - f(x) = k");
-    printf("\n 2 - f(x) = x^k");
-    printf("\n 3 - f(x) = k^x");
-    printf("\n 4 - f(x) = e^x");
-    printf("\n 5 - f(x) = logk(x)");
-    printf("\n 6 - f(x) = ln(x)");
-    printf("\n 7 - f(x) = sen(x)");
-    printf("\n 8 - f(x) = cos(x)");
-    printf("\n 9 - f(x) = tg(x)");
-    printf("\n 10 - f(x) = sqrt(x)");
-    printf("\n 11 - f(x) = 1/x\n");
-    printf(" ");
-        scanf("%d", &opf);
-
-    if(opf==1 || opf==2 || opf==3 || opf==5){
-    printf("\n Digite o valor de K:\n ");
-        scanf("%f", &k);
+    }}
+    
+    if(op==2){
+        printf("\n Digite o valor de A:\n ");
+            scanf("%f", &a);
+        printf(" Digite o valor de B:\n ");
+            scanf("%f", &b);
+        
+        if(opf==1 || opf==2 || opf==3 || opf==5){
+            printf("\n Digite o valor de K:\n ");
+                scanf("%f", &k);}
+        result = Integral(opf, k, a, b);
+            printf("\n Integral = %f\n", result);
     }
-
-    result = Integral(opf, k, a, b);
-    printf("\n Integral = %f\n", result);
-    break;
-
-case 3:
-    printf(" Digite o valor de X0 in R:\n ");
-    scanf("%f", &x0);
-    printf(" Escolha a Função abaixo:\n");
-    printf("\n 1 - f(x) = x^2 - 2");
-    printf("\n 2 - f(x) = 2x - cos(x)");
-    printf("\n 3 - f(x) = x . ln(x) - 1\n");
-        scanf("%d", &opf);
+    
+    if(op==3){
+        printf(" Digite o valor de X0 in R:\n ");
+            scanf("%f", &x0);
+        
+        printf(" Escolha a FunÃ§Ã£o abaixo:\n");
+        printf("\n 1 - f(x) = x^2 - 2");
+        printf("\n 2 - f(x) = 2x - cos(x)");
+        printf("\n 3 - f(x) = x . ln(x) - 1\n");
+            scanf("%d", &opf);
 
     resultzero = Zero(x0,opf);
-    printf("\n Solução = %f", resultzero[1] );
+    printf("\n SoluÃ§Ã£o = %f", resultzero[1] );
     printf("\n Erro = %f", resultzero[0]);
-
-    break;
-case 4:
-    printf("-----------------Obrigado por utilizar nossa calculadora! -------------------");
-    break;
-default:
-    printf(" Opção incorreta. ");
-    break;
     }
- }while(op > 5);
+    printf("Deseja efetuar outro calculo?\n");
+    printf("S - Voltar ao menu");
+    printf("N - Sair");
+    final = getchar();
+  }while(final=='S');
 
     return 0;
 }
