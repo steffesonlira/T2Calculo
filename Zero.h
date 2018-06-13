222d78075e9035d1e6c64740f52bb17e2556160f
steffesonlira <stefferson30@hotmail.com>
2018-06-13 00:19:36 -0300

    Updated document
    Atualizado documento 13-06-18

--- Zero.h
+++ Zero.h
@@ -1,7 +1,7 @@
-//Protótipo
-double * Zero (double xz, int opt);
+//ProtÃ³tipo
+double * Zero (double xz, char opt);
 
-double * Zero (double xz, int opt){
+double * Zero (double xz, char opt){
 int i,k=0;
 double result[2];
 double e = pow(10,-6);
@@ -16,7 +16,7 @@ vetorx[0] = xz;
 
 switch(opt){
 
-case 1: //f(x) = x^2 - 2
+case '1': //f(x) = x^2 - 2
     for(i=0;i<51;i++){
        derivada = (((pow(vetorx[i]+h,2))-2) - (pow(vetorx[i],2)-2) )/ h;
        vetorx[i+1] = vetorx[i] - ((pow(vetorx[i],2) - 2) / derivada );
@@ -30,7 +30,8 @@ case 1: //f(x) = x^2 - 2
     result[0] = erro;
     result[1] = vetorx[k];
     break;
-case 2: //2x - cos(x)
+
+case '2': //2x - cos(x)
     for(i=0;i<51;i++){
        derivada = (((2*vetorx[i]+h) - cos(vetorx[i]+h))- (2*vetorx[i] - cos(vetorx[i])))/h;
        vetorx[i+1] = vetorx[i] - ((2*vetorx[i] - cos(vetorx[i])) / derivada );
@@ -44,11 +45,14 @@ case 2: //2x - cos(x)
     result[0] = erro;
     result[1] = vetorx[k];
     break;
-case 3: //x . ln(x) - 1
+
+case '3': //x . ln(x) - 1
     for(i=0;i<51;i++){
+
        derivada = (((vetorx[i]+h)*log(vetorx[i]+h) -1) - (vetorx[i]*log(vetorx[i]) -1))/h;
        vetorx[i+1] = vetorx[i] - ( (vetorx[i]*log(vetorx[i]) -1)   / derivada );
        erro = (vetorx[i+1] - vetorx[i]) / vetorx[i+1];
+
        if(erro<0){
         erro = erro * -1;
        }
